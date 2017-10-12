6b5c7d3b461ddb3c9774b006fd5438a13beb0fb9
Jagadeeswaraiah Palle <jpalle@lm-maa-00668867.paypalcorp.com>
2017-10-12 10:46:17 +0530

    Initial Commit

---
+++ WKWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKWatch
+//
+//  Created by Palle, Jagadeeswaraiah(AWF) on 20/07/1939 Saka.
+//  Copyright © 1939 Saka PayPal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
